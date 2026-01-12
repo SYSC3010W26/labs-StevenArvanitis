d092fdafba3a6c0c2f2fadec15bab74a69e0183a
Steven Arvanitis <stevenarvanitis@cmail.carleton.ca>
2026-01-12 00:28:55 -0500

    feat: Add name scrolling script

--- Lab1/showmyname.py
+++ Lab1/showmyname.py
@@ -1,12 +1,1 @@
-from sense_hat import SenseHat
-from time import sleep
 
-sense = SenseHat()
-
-blue = (0, 0, 255)
-yellow = (255, 255, 0)
-
-while True:
-    sense.show_message("Steven Arvanitis", scroll_speed=0.05,
-                       text_colour=yellow, back_colour=blue)
-    sleep(0.5)
